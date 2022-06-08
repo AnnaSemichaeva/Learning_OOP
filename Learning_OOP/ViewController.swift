8deeece1f65646880b314f1899ffe1609c17fb02
AnnaSemichaeva <tdhnss@gmail.com>
2022-06-08 23:24:42 +0300

    Initial commit

---
+++ Learning_OOP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Learning_OOP
+//
+//  Created by macuser on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
